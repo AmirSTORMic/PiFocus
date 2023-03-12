e2778c85a94f5af93b2ded2ac126dffacc47dece
AmirSTORMic <80147442+AmirSTORMic@users.noreply.github.com>
2023-03-12 11:54:07 +0000

    Update πFocusPiezo.py

--- πFocusPiezo.py
+++ πFocusPiezo.py
@@ -17,33 +17,30 @@ import Adafruit-MCP4725 # pip install Adafruit-MCP4725
 __author__ = 'Amir Rahmani'
 __version__ = '0.2.0'
 __license__ = 'University of Leeds'
-# -----------------------------------------------------------------------------
-def gaussianbeam(xdata, i0, x0, y0, sX, sY, amp):
+
+# ----------------------------------------------------------------------------------------------------
+# Define the function that is going to be used to fit on the data. In our case, a 2D Gaussian profile.
+# ----------------------------------------------------------------------------------------------------
+def GaussianBeam(xdata, i0, x0, y0, sX, sY, amp):
     (x, y) = xdata
     x0 = float(x0)
     y0 = float(y0)
     eq =  i0+amp*np.exp(-((x-x0)**2/2/sX**2 + (y-y0)**2/2/sY**2))
     return eq.ravel()
-# -----------------------------------------------------------------------------
-def GCurFit(dir_path, init_guess):
+
+# ----------------------------------------------------------------------------------------------------
+# This function applies the fitting algorithm on the beam profile and will give the calibration plots.
+# ----------------------------------------------------------------------------------------------------
+def GCurFit(rootPath, init_guess, ScanRange, StepSize):
     x_sigma = []
     y_sigma = []
     i_values = []
-    count = 0
-    
-    # Iterate directory to count the number of captures
-    for path in os.listdir(dir_path):
-        # check if current path is a file
-        if os.path.isfile(os.path.join(dir_path, path)):
-            count += 1
-        # print('File count:', count)
+    frame = ScanRange/(StepSize*0.001)
     
     # To read the acquired images and apply the Gaussian fitting
-    for i in range(1,count):
-        t = time.time()
+    for i in range(1,frame):
         i_values.append(i)
-        
-        stacks = dir_path
+        stacks = rootPath+'Test'+str(i)+'.tiff'
         img = cv2.imread(stacks)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         im = np.asarray(img).astype(float)
@@ -51,7 +48,7 @@ def GCurFit(dir_path, init_guess):
         h1, w1 = im.shape
         x, y = np.meshgrid(np.arange(w1),np.arange(h1))
         
-        popt, pcov = curve_fit(gaussianbeam, (x, y), im.ravel(), p0=init_guess, maxfev = 5000)
+        popt, pcov = curve_fit(GaussianBeam, (x, y), im.ravel(), p0=init_guess, maxfev = 10000)
         
         init_guess.clear()
         init_guess.append(popt)
@@ -61,19 +58,29 @@ def GCurFit(dir_path, init_guess):
         
         x_sigma.append(popt[3])
         y_sigma.append(popt[4])
+          
+    # This is just to set the x-axis of the graph to the axial values
+    i_values = np.array(i_values)
+    z_values = i_values*StepSize
+    z_values = z_values.tolist() # This should be based on True/False direction
 
-    # Plot the curves        
-    plt.plot(i_values, x_sigma, 'r*', markersize=4, label="x width")
-    plt.plot(i_values, y_sigma, 'b*', markersize=4, label="y width")
-    plt.plot(i_values, np.subtract(x_sigma,y_sigma), 'kx', markersize=4)
-    
+    # Plot the curves
+    # with plt.xkcd(): 
+    plt.plot(z_values, x_sigma, 'r*', markersize=4, label="x width")
+    plt.plot(z_values, y_sigma, 'b*', markersize=4, label="y width")
+    plt.plot(z_values, np.subtract(x_sigma,y_sigma), 'kx', markersize=4)
+    #slopeX, interceptX = np.polyfit(np.log(x_sigma), np.log(z_values), 1)
+    #plt.text(.5, 80, r'$\sigma_y=10,\ \sigma_x=10$')
     plt.grid(True)
-    
-    plt.xlabel("Time")
-    plt.ylabel("Beam Profile Width")
+    #plt.xlim(2, 5.5)
+    plt.xlabel("Z Values (um)")
+    plt.ylabel("Beam Width")
     plt.legend()
     plt.show()
-# -----------------------------------------------------------------------------
+    
+# ----------------------------------------------------------------------------------------------------
+# A function to do a z scan using the Piezo
+# ----------------------------------------------------------------------------------------------------
 def ZPiezoCtrl(scanrange, step_size, direction, VDD):
     print("Scan range should be in um and step size should be in nm.")
     # DAC settings
@@ -95,10 +102,12 @@ def ZPiezoCtrl(scanrange, step_size, direction, VDD):
     I.e. the output voltage is the VDD reference scaled by value/4096. If persist is true it will save the voltage value in EEPROM 
     so it continues after reset (default is false, no persistence).
     """
-for i in range(1, NumSteps):
-    MCP_DAC.raw_value = (MCP_DAC.raw_value + StepValue)
-    time.sleep(1.0)
-# -----------------------------------------------------------------------------    
+    for i in range(1, NumSteps):
+        MCP_DAC.raw_value = (MCP_DAC.raw_value + StepValue)
+        time.sleep(1.0)
+        
+# ----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 def AcqPiFocus(timepoints):
     directory = time.strftime("%Y%m%d-%H%M%S")+"_CL500mm_40X"
     parent_dir = "/home/ponjaviclab/Downloads/" # depends on your system, you need to change this.
@@ -115,8 +124,8 @@ def AcqPiFocus(timepoints):
         picam2.capture_file(Acq_path+filename)
         GCurFit(Acq_path+filename, [2,370,370,380,380,150])
         if ((np.subtract(x_sigma,y_sigma)>10) and (np.subtract(x_sigma,y_sigma)>0)):
-            StepperCtrl(NumSteps, False)
+            ZPiezoCtrl(NumSteps, False)
         elif ((np.subtract(x_sigma,y_sigma)>10) and (np.subtract(x_sigma,y_sigma)<0)):
-            StepperCtrl(NumSteps, False)
+            ZPiezoCtrl(NumSteps, False)
         else:
             print("Focus is locked!")
