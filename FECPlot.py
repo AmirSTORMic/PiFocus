06663bedbaf4b4b78a9e82c4618cea36d3d51bfd
Amir Rahmani <80147442+AmirSTORMic@users.noreply.github.com>
2023-08-13 13:38:56 +0100

    Update and rename GCurFit.py to FECPlot.py

--- FECPlot.py
+++ FECPlot.py
@@ -1,5 +1,5 @@
 """
-This code is used to obtain z-calibration curves for the acquired data on the implemented focus stabilisation system. 
+This code was used to obtain z-calibration curves for the acquired data on the implemented focus stabilisation system. 
 Authors: Amir Rahmani, Tabitha Cox, Aleks Ponjavic
 Affiliation: School of Physics and Astronomy, University of Leeds, Leeds, UK
 """
@@ -8,7 +8,7 @@ Affiliation: School of Physics and Astronomy, University of Leeds, Leeds, UK
 import os
 import cv2
 import time
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -20,34 +20,41 @@ def gaussianbeam(xdata, i0, x0, y0, sX, sY, amp):
     eq =  i0+amp*np.exp(-((x-x0)**2/2/sX**2 + (y-y0)**2/2/sY**2))
     return eq.ravel()
 
-# This function applies the fitting algorithm on the beam profile and will give the calibration plots.
-def GCurFit(dir_path, init_guess, ScanRange):
-    x_sigma = []
-    y_sigma = []
-    i_values = []
+# Iterate directory to count the number of captures
+def numFiles(dir_path):
     count = 0
-    
-    # Iterate directory to count the number of captures
     for path in os.listdir(dir_path):
         if os.path.isfile(os.path.join(dir_path, path)):
             count += 1
         # print('File count:', count)
-        
+    return count
+
+# This function applies the fitting algorithm on the beam profile and will give the calibration plots.
+def FECPlot(dLoc, init_guess, ScanRange, StepSize):
+    x_sigma = []
+    y_sigma = []
+    i_values = []
+    dp = 'C:/Users/44785/'
+    dir_Path = dp+dLoc
+    count = numFiles(dir_Path)
+    # init_guess = [1000,400,400,100,100,400]
+    
     # To read the acquired images and apply the Gaussian fitting
     for i in range(1,count):
-        
         i_values.append(i)
-        
-        stacks = dir_path+'Test'+str(i)+'.tiff'
-        img = cv2.imread(stacks)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        stacks = dir_Path+'Test'+str(i).zfill(3)+'.tif'
+        img = cv2.imread(stacks, -1)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
         im = np.asarray(img).astype(float)
+        #im = im[150:350, 300:500] # if you need to crop the images, uncomment this line. 
         
         h1, w1 = im.shape
         x, y = np.meshgrid(np.arange(w1),np.arange(h1))
         
-        popt, pcov = curve_fit(gaussianbeam, (x, y), im.ravel(), p0=init_guess, maxfev = 10000)
+        popt, pcov = curve_fit(gaussianbeam, (x, y), im.ravel(), p0=init_guess, maxfev = 150000)
+        popt[3] = np.abs(popt[3])
+        popt[4] = np.abs(popt[4])
         
         init_guess.clear()
         init_guess.append(popt)
@@ -58,14 +65,19 @@ def GCurFit(dir_path, init_guess, ScanRange):
         x_sigma.append(popt[3])
         y_sigma.append(popt[4])
         
-      
-    # This is just to set the x-axis of the graph to the axial values
-    StepSize = ScanRange/count
+        # plt.plot((popt[1],popt[1]+popt[3]),(popt[2],popt[2]))
+        # plt.plot((popt[1],popt[1]),(popt[2],popt[2]+popt[4]))
+        # plt.imshow(im)
+        # plt.show()
+
+    " To set the x-axis of the graph to the axial values "
+    """ ------------------------------------------------------------------ """
     i_values = np.array(i_values)
     z_values = i_values*StepSize
-    z_values = z_values.tolist() # This should be based on a True/False direction
+    z_values = z_values.tolist() # Based on a True/False direction
     
     "Standard Deviation Calculation"
+    """ ------------------------------------------------------------------ """
     # See the link below for a reference of the standard deviation formula
     # https://www.mathsisfun.com/data/standard-deviation-formulas.html
     # Convert the lists to arrays
@@ -76,7 +88,7 @@ def GCurFit(dir_path, init_guess, ScanRange):
     
     # Calculate the mean of the values in your array
     mean_DiffArr = sum(DiffArr) / len(DiffArr)
-
+    
     # Calculate the variance of the values in your array
     # This is 1/N * sum((x - mean(X))^2)
     var_DiffArr = sum((x - mean_DiffArr) ** 2 for x in DiffArr) / len(DiffArr)
@@ -85,23 +97,25 @@ def GCurFit(dir_path, init_guess, ScanRange):
     sd_XY = var_DiffArr ** 0.5
     print('This is the standard deviation:', sd_XY)
     
-    # Plot the curves
+    "Plot the curves"
+    """ ------------------------------------------------------------------ """
     # with plt.xkcd(): 
-    plt.plot(z_values, x_sigma, 'r*', markersize=4, label="x width")
-    plt.plot(z_values, y_sigma, 'b*', markersize=4, label="y width")
-    plt.plot(z_values, np.subtract(x_sigma,y_sigma), 'kx', markersize=4)
+    plt.plot(z_values, x_sigma, 'ro', markerfacecolor='none', markersize=4, label="x width")
+    plt.plot(z_values, y_sigma, 'bo', markerfacecolor='none', markersize=4, label="y width")
+    plt.plot(z_values, np.subtract(x_sigma,y_sigma), 'ko', markerfacecolor='none', markersize=4)
     #slopeX, interceptX = np.polyfit(np.log(x_sigma), np.log(z_values), 1)
     #plt.text(.5, 80, r'$\sigma_y=10,\ \sigma_x=10$')
     plt.grid(True)
-    #plt.xlim(2, 5.5)
-    plt.xlabel("Z Values")
-    plt.ylabel("Beam Profile Width")
+    plt.minorticks_on()
+    plt.xlabel("Z Values (µm)")
+    plt.ylabel("Beam Width (µm)")
     plt.legend()
+    plt.title("dLoc")
     plt.show()
     
 # Run the following commands to see the folders in the directory.
-# dp = r'/Projects/Autofocus/Wolfson/'
+# dp = r'/Projects/Autofocus/'
 # [x[0] for x in os.walk(dp)]
 
-# GCurFit(dir_path, init_guess, ScanRange)
+# FEC(dLoc, init_guess, ScanRange, StepSize)
 # This will give the calibration curve for the dataset of interest.
